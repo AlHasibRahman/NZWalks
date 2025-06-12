702d2bf692232a257443a4fa0f473eae8dcb6c02
Hasib Rahman <rahmanalhasibur@gmail.com>
2025-06-12 20:39:58 +0600

    Create DbContext & Perform Migrations

---
+++ NZWalks/Data/NZWalksDbContext.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NZWalks.Models.Domain;
+
+namespace NZWalks.Data
+{
+    public class NZWalksDbContext : DbContext
+    {
+        public NZWalksDbContext(DbContextOptions<NZWalksDbContext> dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+
+        //Dbset properties
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+    }
+}
--- NZWalks/Program.cs
+++ NZWalks/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using NZWalks.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
+builder.Services.AddDbContext<NZWalksDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("NZWalkConnectionString")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
